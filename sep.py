5b764c8e985fee7bbd5ff59c24da93297f0d78aa
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-09 19:27:05 +0530

    Add files via upload

---
+++ sep.py
@@ -0,0 +1,14 @@
+'''
+DAY: WEDNESDAY
+DATE: 09TH NOV 2022
+TOPIC: SEPERATION
+AUTHOR:pooja'''
+print('hello'   ,  'world') #hello world
+print('hello' +' '+   'world')#hello world
+print('hello'+chr(32)+'world')#hello world
+
+print('python'+"-"+'program'+'-'+'world')#python-program-world
+
+print('hello', 'world',sep="*")#hello*world
+
+print('p','y','t','h','o','n',sep='@') #p@y@t@h@o@n
